c220bc24b4378d604d3f6e1e3a448f5178d77da6
Marco Papula <ga84div@mytum.de>
2020-05-10 01:44:54 +0200

    update example

--- example/main.dart
+++ example/main.dart
@@ -7,27 +7,40 @@ class Person extends Codable {
 
   String name;
   int age;
-  double score;
+  Grades grades;
 }
 
-String jsonString = "{ \"name\": \"Marco\", \"age\": 24, \"score\": 99.9 }";
+class Grades extends Codable {
+  Grades.decode(Map<String, dynamic> data) : super.decode(data);
+
+  String subject;
+  double grade;
+}
+
+String jsonString =
+    "{ \"name\": \"Marco\", \"age\": 24, \"grades\": { \"subject\": \"Math\", \"grade\": 1.0 } }";
 String corruptedJsonString =
     "{ \"notname\": \"Marco\", \"age\": 24, \"score\": 99.9 }";
 
 void main() {
-  Person me = Person.decode(json.decode(jsonString));
-
-  print("Name: ${me.name}");
-  print("Age: ${me.age}");
-  print("Score: ${me.score}");
-  print("");
-
-  print(json.encode(me.encode()));
-  print("");
-
   try{
+    Person me = Person.decode(json.decode(jsonString));
+
+    print("Name: ${me.name}");
+    print("Age: ${me.age}");
+    print("Grades subject: ${me.grades.subject}");
+    print("Grades grade: ${me.grades.grade}");
+    print("");
+
+    print(json.encode(me.encode()));
+    print("");
+  } catch(e) {
+    print(e);
+  }
+
+  try {
     Person.decode(json.decode(corruptedJsonString));
-  } catch(e){
+  } catch (e) {
     print(e);
   }
 }
